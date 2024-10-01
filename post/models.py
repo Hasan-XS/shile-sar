de011670417c4c81095597b530348853b674ab89
Hasan-XS <ha3an.majidii@gmail.com>
2024-10-01 11:59:07 -0700

    'Create'

--- post/models.py
+++ post/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    
+    def __str__(self):
+        return self.name
+    
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=230, unique=False)
+    content = models.TextField(unique=False)
+    category = models.ForeignKey(Category, default=1, on_delete=models.CASCADE)
+    time = models.DateField()
+    author = models.ForeignKey(User, default=1, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.title
--- post/views.py
+++ post/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
 
+
 def index_view(request):
-    return HttpResponse("index")
+    posts = list(reversed(Post.objects.all()))
+    return render(
+        request,
+        "index.html",
+        {
+            "posts": posts,
+        },
+    )
+
