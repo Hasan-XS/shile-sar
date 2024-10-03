3ad47b401adafcc486cee988ad38756837b6ad71
Hasan-XS <ha3an.majidii@gmail.com>
2024-10-03 11:38:10 -0700

    'Create'

--- post/urls.py
+++ post/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import index_view
+from .views import index_view, posts_by_category
 
 urlpatterns = [
-    path('', index_view, name="index_home"),
+    path("", index_view, name="index_home"),
+    path(
+        "posts/category/<str:category_name>/",
+        posts_by_category,
+        name="posts_by_category",
+    ),
 ]
--- post/views.py
+++ post/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import *
 
@@ -15,3 +15,17 @@ def index_view(request):
         },
     )
 
+
+def posts_by_category(request, category_name):
+    category = get_object_or_404(Category, name=category_name)
+
+    posts = list(reversed(Post.objects.filter(category=category)))
+
+    return render(
+        request,
+        "post/posts_by_category.html",
+        {
+            "posts": posts,
+            "category": category,
+        },
+    )
